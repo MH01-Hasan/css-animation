a682323c2a3848ed506d7f0020ab401c1d20791e
Mehedi hasan <86625015+MH01-Hasan@users.noreply.github.com>
2023-08-24 14:50:36 +0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+const names = ['alif','tammana','mehedi','sanjana','alif','sanjana']
+
+
+function duplicatevlue (names){
+    const unique = []
+
+    for(let i = 0; i < names.length; i++){
+        const name = names[i]
+
+        if(unique.includes(name) ===false){
+            unique.push(name)
+        }
+    }
+    return unique
+
+}
+
+const value =  duplicatevlue(names)
+console.log(value)
