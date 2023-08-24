bfb18ec99c9ba9708b1ef14fadebd95b13f2ec61
Mehedi hasan <86625015+MH01-Hasan@users.noreply.github.com>
2023-08-24 15:30:21 +0600

    Update index.js

--- index.js
+++ index.js
@@ -17,3 +17,42 @@ function duplicatevlue (names){
 
 const value =  duplicatevlue(names)
 console.log(value)
+
+
+const phones =[
+{
+    name:"walton",
+    price:1200,
+    color:"selver"
+},
+{
+    name:"samsung",
+    price:600,
+    color:"selver"
+},
+{
+    name:"mi",
+    price:1500,
+    color:"selver"
+},
+{
+    name:"walton",
+    price:1600,
+    color:"selver"
+}
+
+]
+
+function chipestphone (phones){
+    let chipest = phones[0]
+    for(let i =0 ; i < phones.length; i++){
+        const phone = phones[i]
+
+        if(phone.price > chipest.price ){
+            chipest = phone
+        }     
+    }
+    return chipest
+}
+const chip = chipestphone(phones)
+console.log(chip)
